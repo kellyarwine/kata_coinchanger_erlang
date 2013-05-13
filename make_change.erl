7d0e29f06d92ef46eac604d85a13aa0e1db28482
Kelly Steensma <kellysteensma@gmail.com>
2013-05-13 07:47:42 -0500

    Refactored to cycle through set list of coins.

--- make_change.erl
+++ make_change.erl
@@ -1,32 +1,16 @@
 -module(make_change).
--export([get/1, get/2]).
+-export([get/1, get/3]).
 -define(COINS, [25,10,5,1]).
 
-
-
 get(Amount) ->
-  get(Amount, []).
+  get(Amount, [], ?COINS).
 
+get(Amount, Change, [Curr_Coin|Rem_Coin]) when Amount >= Curr_Coin ->
+  New_Change = Change ++ [Curr_Coin],
+  get(Amount - Curr_Coin, New_Change, [Curr_Coin|Rem_Coin]);
 
+get(Amount, Change, [Curr_Coin|Rem_Coin]) ->
+  get(Amount, Change, Rem_Coin);
 
-get(Amount, Change) when Amount >= 25 ->
-  New_Change = Change ++ [25],
-  Amount - 25,
-  get(Amount - 25, New_Change);
-
-get(Amount, Change) when Amount >= 10 ->
-  New_Change = Change ++ [10],
-  Amount - 10,
-  get(Amount - 10, New_Change);
-
-get(Amount, Change) when Amount >= 5 ->
-  New_Change = Change ++ [5],
-  Amount - 5,
-  get(Amount - 5, New_Change);
-
-get(Amount, Change) when Amount > 0 ->
-  New_Change = Change ++ [1],
-  get(Amount - 1, New_Change);
-
-get(_, Change) ->
+get(_, Change, [])->
   Change.
